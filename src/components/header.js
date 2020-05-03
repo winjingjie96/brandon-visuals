b07f1c05a8d98356441629c5093368d6a55a481e
Jing Jie <jj@hireplace.com>
2020-05-03 19:37:39 +0800

    added netlify identity

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import netlifyIdentity from "netlify-identity-widget"
+
 
 import gatsbyLogo from "../images/gatsby-icon.png"
 
@@ -11,7 +13,13 @@ const isActive = ({ isCurrent }) => {
 const NavLink = props => <Link getProps= {isActive} {...props} />
 
 
-const Header = ({ siteTitle }) => (
+class Header extends React.Component{
+  componentDidMount () {
+    netlifyIdentity.init()
+  }
+  render () {
+    const {siteTitle} = this.props
+  return(
   <header
     style={{
       background: `rebeccapurple`,
@@ -39,6 +47,8 @@ const Header = ({ siteTitle }) => (
 
 <NavLink to = "/blog">Blog</NavLink>
 <NavLink to = "/products">Store</NavLink>
+<div data-netlify-identity-menu></div>
+
 
 
       {/* Shopping Cart Summart */}
@@ -52,7 +62,7 @@ const Header = ({ siteTitle }) => (
       </div>
     </div>
   </header>
-)
+)}}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
